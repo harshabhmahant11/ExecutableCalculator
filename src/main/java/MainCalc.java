b51edc79f05010dc0a2e09697a8c91f9abb8e7a4
Harshabh Mahant <harshabhmahant11@gmail.com>
2020-04-23 10:38:42 +0530

    Added multiply functionality

--- src/main/java/MainCalc.java
+++ src/main/java/MainCalc.java
@@ -3,8 +3,12 @@ public class MainCalc {
     public static void main(String[] args) {
         System.out.println("6 + 3 = " + add(6, 3));
         System.out.println("6 - 3 = " + subtract(6, 3));
-//		System.out.println("6 * 3 = " + multiply(6,3));
+		System.out.println("6 * 3 = " + multiply(6,3));
 //		System.out.println("6 / 3 = " + divide(6,3));
+    }
+
+    public static int multiply(int a, int b) {
+        return a*b;
     }
 
     public static int add(int a, int b) {
