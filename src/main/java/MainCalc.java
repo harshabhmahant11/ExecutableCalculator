738e5adb5ab42a8df3a0fd5d17de6c05016282cd
Harshabh <harshabhmahant11@gmail.com>
2020-05-04 17:41:37 +0530

    added division functionality

--- src/main/java/MainCalc.java
+++ src/main/java/MainCalc.java
@@ -4,7 +4,7 @@ public class MainCalc {
         System.out.println("6 + 3 = " + add(6, 3));
         System.out.println("6 - 3 = " + subtract(6, 3));
 		System.out.println("6 * 3 = " + multiply(6,3));
-//		System.out.println("6 / 3 = " + divide(6,3));
+		System.out.println("6 / 3 = " + divide(6,3));
     }
 
     public static int multiply(int a, int b) {
@@ -18,5 +18,15 @@ public class MainCalc {
     public static int subtract(int a, int b) {
         return a - b;
     }
+
+    public static double divide(int a, int b) {
+		double result;
+		if (b == 0) {
+			throw new IllegalArgumentException("Divisor cannot divide by zero");
+		} else {
+			result = Double.valueOf(a)/Double.valueOf(b);
+		}
+		return result;
+	}
 
 }
